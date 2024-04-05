b1899a1ec49b9bfda8be3ca6757f9f38ec3c5307
Benjamin Czaja <benjamic@tcn1.local.snellius.surf.nl>
2024-04-05 18:19:04 +0200

    updating scripts

--- tutorials/monitoring_ear/NPB_job.sh
+++ tutorials/monitoring_ear/NPB_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -p genoa
+#SBATCH -p rome
 #SBATCH -t 00:15:00
 #SBATCH --nodes=1
 #SBATCH --exclusive
@@ -19,6 +19,6 @@ module load foss/2023a
 # |   D   |      1632      |      1216      |       34       |
 
 
-srun --ntasks=192 /projects/0/energy-course/NPB3.4-MZ-MPI/sp-mz.C.x
+srun --ntasks=128 /projects/0/energy-course/NPB3.4-MZ-MPI/sp-mz.C.x
 
-srun --ntasks=192 /projects/0/energy-course/NPB3.4-MZ-MPI/sp-mz.D.x
+srun --ntasks=128 /projects/0/energy-course/NPB3.4-MZ-MPI/sp-mz.D.x
