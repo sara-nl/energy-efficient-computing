dc4a2c7de04a6e7bf15db1741312769c79ac5ca5
Benjamin Czaja <benjamic@int6.local.snellius.surf.nl>
2024-04-12 11:00:35 +0200

    updating NPB policy script

--- tutorials/policies/NPB_job.sh
+++ tutorials/policies/NPB_job.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-#SBATCH -p rome
+#SBATCH -p genoa
 #SBATCH -t 00:35:00
 #SBATCH --nodes=1
 #SBATCH --exclusive
 
-#SBATCH --output=NPB.out
-#SBATCH --error=NPB.err
+#SBATCH --output=NPB.%j.out
+#SBATCH --error=NPB.%j.err
 #SBATCH --job-name=NPB
 
 module load 2023
 module load foss/2023a
+
+# ENV variable needed to report "loops" to the EARDB
+# export EARL_REPORT_LOOPS=1
+
+# location of the binaries for the course
+PROJECT_DIR=/projects/0/energy-course
 
 # Two Class sizes available 
 
@@ -18,6 +24,6 @@ module load foss/2023a
 # |   C   |       240      |       320      |       28       |
 # |   D   |      1632      |      1216      |       34       |
 
-srun --ear=on --ear-policy=monitoring --ntasks=128 /projects/0/energy-course/NPB3.4-MZ-MPI/sp-mz.D.x
-srun --ear=on --ear-policy=min_time --ntasks=128 /projects/0/energy-course/NPB3.4-MZ-MPI/sp-mz.D.x
-srun --ear=on --ear-policy=min_energy --ntasks=128 /projects/0/energy-course/NPB3.4-MZ-MPI/sp-mz.D.x
+srun --ear=on --ear-policy=monitoring --ntasks=192 /projects/0/energy-course/NPB3.4-MZ-MPI/sp-mz.D.x
+srun --ear=on --ear-policy=min_time --ntasks=192 /projects/0/energy-course/NPB3.4-MZ-MPI/sp-mz.D.x
+srun --ear=on --ear-policy=min_energy --ntasks=192 /projects/0/energy-course/NPB3.4-MZ-MPI/sp-mz.D.x
